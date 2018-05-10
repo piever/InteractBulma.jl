597ef812fac8ea2ff075c4f26433c9b3d66a495c
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-10 16:21:13 +0100

    add fonts

--- src/InteractBulma.jl
+++ src/InteractBulma.jl
@@ -10,7 +10,8 @@ InteractNative.libraries(::Bulma) = [
         "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css",
         "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-slider/dist/bulma-slider.min.css",
         "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-switch/dist/bulma-switch.min.css",
-        "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-checkradio/dist/bulma-checkradio.min.css"
+        "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-checkradio/dist/bulma-checkradio.min.css",
+        "https://use.fontawesome.com/releases/v5.0.7/js/all.js"
     ]
 
 InteractNative.set_backend(Bulma())
