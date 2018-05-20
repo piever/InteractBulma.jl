2e47df3348627582d99d60a3743552cce1b8625f
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-20 13:13:50 +0100

    form layout

--- src/widgets.jl
+++ src/widgets.jl
@@ -1,3 +1,9 @@
+function wrap(ui, f = dom"div.field")
+    wrapped_ui = f(ui)
+    InteractBase.primary_obs!(wrapped_ui, observe(ui))
+    wrapped_ui
+end
+
 function filepicker(::Bulma, args...; class="", label = "Choose a file...", kwargs...)
     postprocess = t ->
     dom"div.file"(
@@ -14,29 +20,29 @@ function filepicker(::Bulma, args...; class="", label = "Choose a file...", kwar
             dom"span.file-name"("{{filename == '' ? 'No file chosen' : filename}}")
         )
     )
-    filepicker(NativeHTML(), args...; postprocess = postprocess, class="file-input $class", kwargs...)
+    filepicker(NativeHTML(), args...; postprocess = postprocess, class="file-input $class", kwargs...) |> wrap
 end
 
-autocomplete(::Bulma, args...; class="", kwargs...) = autocomplete(NativeHTML(), args...; class="input $class", kwargs...)
+autocomplete(::Bulma, args...; class="", kwargs...) =
+    autocomplete(NativeHTML(), args...; class="input $class", kwargs...) |> wrap
 
 dropdown(::Bulma, options::Associative; class="", kwargs...) =
-    dropdown(NativeHTML(), options; postprocess = dom"div.select", class="input $class", kwargs...)
+    dropdown(NativeHTML(), options; postprocess = dom"div.select", class="input $class", kwargs...) |> wrap
 
 checkbox(::Bulma, args...; class="", kwargs...) =
-    checkbox(NativeHTML(), args...; class="is-checkradio $class", kwargs...)
+    checkbox(NativeHTML(), args...; class="is-checkradio $class", kwargs...) |> wrap
 
 toggle(s::Bulma, args...; class="", kwargs...) =
-    checkbox(NativeHTML(), args...; class="switch $class", kwargs...)
+    checkbox(NativeHTML(), args...; class="switch $class", kwargs...) |> wrap
 
 textbox(::Bulma, label=""; value="", class="", kwargs...) =
-    input(NativeHTML(), value; typ="text", class="input $class", placeholder=label, kwargs...)
+    input(NativeHTML(), value; typ="text", class="input $class", placeholder=label, kwargs...) |> wrap
 
-function slider(::Bulma, args...; class="is-fullwidth", kwargs...)
-    slider(NativeHTML(), args...; class="slider $class", kwargs...)
-end
+slider(::Bulma, args...; class="is-fullwidth", kwargs...) =
+    slider(NativeHTML(), args...; class="slider $class", kwargs...) |> wrap
 
 button(::Bulma, args...; class= "is-primary", kwargs...) =
-    button(NativeHTML(), args...; class="button $class", kwargs...)
+    button(NativeHTML(), args...; class="button $class", kwargs...) |> wrap
 
 input(::Bulma, args...; class="", kwargs...) =
-    input(NativeHTML(), args...; class="input $class", kwargs...)
+    input(NativeHTML(), args...; class="input $class", kwargs...) |> wrap
