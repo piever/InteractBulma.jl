0b19c77f2529c1446884c1edc98e59cb32056ea0
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-14 15:37:49 +0100

    allow kwargs in dropdown

--- src/widgets.jl
+++ src/widgets.jl
@@ -1,5 +1,5 @@
-function dropdown(::Bulma, options::Observable; multiple = false, className = "")
+function dropdown(::Bulma, options::Observable; multiple = false, className = "", kwargs...)
     className = InteractBase.mergeclasses(className, multiple ? "select is-multiple" : "select")
     div_select = Node(:div, className=className)
-    dropdown(NativeHTML(), options; multiple = multiple, div_select = div_select)
+    dropdown(NativeHTML(), options; multiple = multiple, div_select = div_select, kwargs...)
 end
