f1e86cbdc08ab3543ba28916e50d9b2d4f8c5ba0
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-03-30 13:30:59 +0000

    download sass files, add support for custom style (#23)
    download sass files, add support for custom style

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
+using Sass
 const _pkg_root = dirname(dirname(@__FILE__))
 const _pkg_deps = joinpath(_pkg_root,"deps")
 const _pkg_assets = joinpath(_pkg_root,"assets")
@@ -5,14 +6,73 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
 deps = [
-    "https://www.gitcdn.xyz/repo/piever/BulmaThemes/v0.1.0/public/styles/main.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-slider/dist/bulma-slider.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-switch/dist/bulma-switch.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-checkradio/dist/bulma-checkradio.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-accordion/dist/bulma-accordion.min.css",
-    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-tooltip/dist/bulma-tooltip.min.css",
+    "bulma.sass",
+    "sass/base/_all.sass",
+    "sass/base/generic.sass",
+    "sass/base/helpers.sass",
+    "sass/base/minireset.sass",
+    "sass/components/_all.sass",
+    "sass/components/breadcrumb.sass",
+    "sass/components/card.sass",
+    "sass/components/dropdown.sass",
+    "sass/components/level.sass",
+    "sass/components/list.sass",
+    "sass/components/media.sass",
+    "sass/components/menu.sass",
+    "sass/components/message.sass",
+    "sass/components/modal.sass",
+    "sass/components/navbar.sass",
+    "sass/components/pagination.sass",
+    "sass/components/panel.sass",
+    "sass/components/tabs.sass",
+    "sass/elements/_all.sass",
+    "sass/elements/box.sass",
+    "sass/elements/button.sass",
+    "sass/elements/container.sass",
+    "sass/elements/content.sass",
+    "sass/elements/form.sass",
+    "sass/elements/icon.sass",
+    "sass/elements/image.sass",
+    "sass/elements/notification.sass",
+    "sass/elements/other.sass",
+    "sass/elements/progress.sass",
+    "sass/elements/table.sass",
+    "sass/elements/tag.sass",
+    "sass/elements/title.sass",
+    "sass/grid/_all.sass",
+    "sass/grid/columns.sass",
+    "sass/grid/tiles.sass",
+    "sass/layout/_all.sass",
+    "sass/layout/footer.sass",
+    "sass/layout/hero.sass",
+    "sass/layout/section.sass",
+    "sass/utilities/_all.sass",
+    "sass/utilities/animations.sass",
+    "sass/utilities/controls.sass",
+    "sass/utilities/derived-variables.sass",
+    "sass/utilities/functions.sass",
+    "sass/utilities/initial-variables.sass",
+    "sass/utilities/mixins.sass",
 ]
+
 for dep in deps
-    download(dep, joinpath(_pkg_assets, splitdir(dep)[2]))
+    path = joinpath(_pkg_assets, split(dep, '/')...)
+    mkpath(splitdir(path)[1])
+    download("https://cdn.jsdelivr.net/npm/bulma@0.7.4/"*dep, path)
+end
+
+extensions = [
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-slider/dist/bulma-slider.sass",
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-switch/dist/bulma-switch.sass",
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-checkradio/dist/bulma-checkradio.sass",
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-accordion/dist/bulma-accordion.sass",
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@1.0.14/bulma-tooltip/dist/bulma-tooltip.sass",
+]
+for extension in extensions
+    download(extension, joinpath(_pkg_assets, splitdir(extension)[2]))
+end
+
+for file in ["main", "main_confined"]
+    sassfile = joinpath(_pkg_assets, "$file.scss")
+    Sass.compile_file(sassfile, joinpath(_pkg_assets, "$file.min.css"), output_style = Sass.compressed)
 end
--- src/InteractBulma.jl
+++ src/InteractBulma.jl
@@ -6,27 +6,13 @@ export Bulma
 
 struct Bulma<:InteractBase.WidgetTheme; end
 
-const main_css = joinpath(@__DIR__, "..", "assets", "main.css")
-const bulma_min_css = joinpath(@__DIR__, "..", "assets", "bulma.min.css")
-const bulma_slider_min_css = joinpath(@__DIR__, "..", "assets", "bulma-slider.min.css")
-const bulma_switch_min_css = joinpath(@__DIR__, "..", "assets", "bulma-switch.min.css")
-const bulma_accordion_min_css = joinpath(@__DIR__, "..", "assets", "bulma-accordion.min.css")
-const bulma_checkradio_min_css = joinpath(@__DIR__, "..", "assets", "bulma-checkradio.min.css")
-const bulma_tooltip_min_css = joinpath(@__DIR__, "..", "assets", "bulma-tooltip.min.css")
+const main_css = joinpath(@__DIR__, "..", "assets", "main.min.css")
+const main_interactbulma_css = joinpath(@__DIR__, "..", "assets", "main_interactbulma.min.css")
 const font_awesome = InteractBase.font_awesome
 
 function InteractBase.libraries(::Bulma)
-    bulmalibs = InteractBase.isijulia() ?
-        [main_css] :
-        [
-            bulma_min_css,
-            bulma_slider_min_css,
-            bulma_switch_min_css,
-            bulma_accordion_min_css,
-            bulma_checkradio_min_css,
-            bulma_tooltip_min_css,
-        ]
-    vcat(font_awesome, InteractBase.style_css, bulmalibs)
+    bulmalib = InteractBase.isijulia() ? main_interactbulma_css : main_css
+    vcat(font_awesome, InteractBase.style_css, bulmalib)
 end
 
 end # module
