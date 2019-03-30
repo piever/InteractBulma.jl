c24d01b0e4704202ae94653643bd5233a57edf42
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-30 13:41:17 +0000

    fix what to load

--- src/InteractBulma.jl
+++ src/InteractBulma.jl
@@ -7,11 +7,11 @@ export Bulma
 struct Bulma<:InteractBase.WidgetTheme; end
 
 const main_css = joinpath(@__DIR__, "..", "assets", "main.min.css")
-const main_interactbulma_css = joinpath(@__DIR__, "..", "assets", "main_interactbulma.min.css")
+const main_confined_css = joinpath(@__DIR__, "..", "assets", "main_confined.min.css")
 const font_awesome = InteractBase.font_awesome
 
 function InteractBase.libraries(::Bulma)
-    bulmalib = InteractBase.isijulia() ? main_interactbulma_css : main_css
+    bulmalib = InteractBase.isijulia() ? main_confined_css : main_css
     vcat(font_awesome, InteractBase.style_css, bulmalib)
 end
 
