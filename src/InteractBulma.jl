2df811392f692c131843deb9803425f35e7ba7aa
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-03-30 15:17:22 +0000

    Add theming support (#24)
    * gitignore
    
    * add theming mechanism
    
    * default to temporary
    
    * clean file before starting
    
    * docs
    
    * support notebook

--- src/InteractBulma.jl
+++ src/InteractBulma.jl
@@ -1,18 +1,36 @@
 module InteractBulma
 
-import InteractBase
+import InteractBase, Sass
 
-export Bulma
+struct BulmaTheme<:InteractBase.WidgetTheme
+    path::String
+end
 
-struct Bulma<:InteractBase.WidgetTheme; end
-
+const examplefolder = joinpath(@__DIR__, "..", "examples")
 const main_css = joinpath(@__DIR__, "..", "assets", "main.min.css")
 const main_confined_css = joinpath(@__DIR__, "..", "assets", "main_confined.min.css")
 const font_awesome = InteractBase.font_awesome
 
-function InteractBase.libraries(::Bulma)
-    bulmalib = InteractBase.isijulia() ? main_confined_css : main_css
+function InteractBase.libraries(b::BulmaTheme)
+    bulmalib = joinpath(b.path, InteractBase.isijulia() ? "main_confined.css" : "main.css")
     vcat(font_awesome, InteractBase.style_css, bulmalib)
+end
+
+const main_scss = joinpath(@__DIR__, "..", "assets", "main.scss")
+const main_confined_scss = joinpath(@__DIR__, "..", "assets", "main_confined.scss")
+const _overrides_scss = joinpath(@__DIR__, "..", "assets", "_overrides.scss")
+const _variables_scss = joinpath(@__DIR__, "..", "assets", "_variables.scss")
+
+function clean_or_copy(src, dest)
+    src === nothing ? open(io -> nothing, dest, "w") : cp(realpath(abspath(src)), dest, force = true)
+end
+
+function compile_theme(output = mktempdir(); overrides = nothing, variables = nothing)
+    clean_or_copy(overrides, _overrides_scss)
+    clean_or_copy(variables, _variables_scss)
+    Sass.compile_file(main_scss, joinpath(output, "main.css"), output_style = Sass.compressed)
+    Sass.compile_file(main_confined_scss, joinpath(output, "main_confined.css"), output_style = Sass.compressed)
+    return BulmaTheme(output)
 end
 
 end # module
